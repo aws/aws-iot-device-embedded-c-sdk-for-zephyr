16e15ccaf04f9db84f15459a9d0e0af42d58badb
Fan <gilbefan@amazon.com>
2021-07-15 17:09:49 -0700

    Adding back in the platform directory for code review. This adds the transport library implementation for Zephyr, which includes mbedtls and plaintext implementations, with a shared socket file.

---
+++ platform/zephyr/clock/clockFilePaths.cmake
@@ -0,0 +1,14 @@
+# This file is to add source files and include directories
+# into variables so that it can be reused from different repositories
+# in their Cmake based build system by including this file.
+#
+# Files specific to the repository such as test runner, platform tests
+# are not added to the variables.
+
+# Backoff Algorithm library source files.
+set( CLOCK_SOURCES
+     "${CMAKE_CURRENT_LIST_DIR}/source/clock.c" )
+
+# Backoff Algorithm library Public Include directories.
+set( CLOCK_INCLUDE_DIRS
+     "${CMAKE_CURRENT_LIST_DIR}/include" )
